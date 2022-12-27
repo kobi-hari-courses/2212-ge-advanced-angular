65fcdeec3e58bf1c58bea2269745a2e1db38e97d
Kobi Hari <kobi2294@yahoo.com>
2022-12-27 10:36:18 +0200

    observables deomnstrated

--- Module 04/fun-with-rxjs/src/app/app.component.ts
+++ Module 04/fun-with-rxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { from, interval, Observable, Observer, of, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fun-with-rxjs';
+    createObserver(id: string): Observer<number> {
+        return {
+            next: (val: number) => console.log(`Observer ${id} next ${val}`), 
+            complete: () => console.log(`Observer ${id} complete`), 
+            error: err => console.log(`Observer ${id} error ${err}`)
+        }
+    }
+
+    createOfObservable(): Observable<number> {
+        return of(1, 2, 3);
+    }
+
+    createFromObservable(): Observable<number> {
+        return from([1, 2, 3]);
+    }
+
+    createTimerObservable(): Observable<number> {
+        return timer(2000, 500);
+    }
+
+    createIntervalObservable(): Observable<number> {
+        return interval(1000);
+    }
+
+    createCustomnObservable(): Observable<number> {
+        return new Observable(observer => {
+            observer.next(42);
+            observer.next(100);
+            setTimeout(() => observer.next(200), 2000);
+            setTimeout(() => observer.next(300), 4000);
+            setTimeout(() => observer.next(400), 6000);
+            setTimeout(() => observer.next(500), 8000);
+            setTimeout(() => observer.complete(), 10000);
+            setTimeout(() => observer.next(600), 13000);
+        })
+    }
+
+
+
+    go() {
+        const observerA = this.createObserver('A');
+        const observerB = this.createObserver('B');
+
+        const observable = this.createCustomnObservable();
+
+        observable.subscribe(observerA);
+
+        setTimeout(() => observable.subscribe(observerB), 2500);
+    }
 }
